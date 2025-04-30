4e7f27fadb50101113db4a8c3d34ff708426bceb
KrinaPatel24 <161822814+KrinaPatel24@users.noreply.github.com>
2025-04-30 13:38:00 +0530

    Left-Section by using props (ProductPage)

--- src/landing_page/products/LeftSection.jsx
+++ src/landing_page/products/LeftSection.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 
 function LeftSection(
-  imageURL,
+ { imageURL,
   productName,
   productDescription,
   tryDemo,
   learnMore,
   googlePlay,
-  appStore
+  appStore}
 ) {
   return (
-    <div className="container">
+    <div className="container border-top mt-3 pt-5">
       <div className="row">
         <div className="col-6 p-3">
           <img src={imageURL}  />
         </div>
-        <div className="col-6">
+        <div className="col-6 p-5">
             <h1>{productName}</h1>
-            <p>{productDescription}</p>
-            <a href={tryDemo}>Try Demo</a>
-            <a href={learnMore}>Learn more</a>
+            <p style={{width:"80%"}} className="mt-4" >{productDescription}</p>
+           <div className="mt-5">
+           <a href={tryDemo}  style={{textDecoration: "none"}}>Try Demo <b>⟶</b></a>
+           <a className="m-3"  style={{textDecoration: "none"}} href={learnMore}>Learn more <b>⟶</b></a>
+           </div>
+            <div  className="mt-5">
             <a href={googlePlay}><img src="media/images/googlePlayBadge.svg" /></a>
-            <a href={appStore}><img src="media/images/googlePlayBadge.svg"  /></a>
+            <a  className="m-3"href={appStore}><img src="media/images/appstoreBadge.svg"  /></a>
+            </div>
         </div>
       </div>
     </div>
