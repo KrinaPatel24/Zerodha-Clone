1dc62532a7371ab6411957177c68c6bdb7ae170d
KrinaPatel24 <161822814+KrinaPatel24@users.noreply.github.com>
2025-05-01 16:22:34 +0530

    PricingPage completed(Hero.jsx and Brokerage.jsx component completed)

--- src/landing_page/pricing/Hero.jsx
+++ src/landing_page/pricing/Hero.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import Hero from './../home/Hero';
+import React from "react";
 
 function Hero() {
-    return ( <h1>Hero</h1> );
+  return (
+    <div className="container mt-5 mb-5">
+      <div className="row border-bottom mt-5 text-center pb-3">
+        <h1>Pricing</h1>
+        <h2 className="mt-3 pb-3 text-muted fs-5">
+          Free equity investments and flat ₹20 traday and F&Q trades
+        </h2>
+      </div>
+      <div className="row mt-5">
+        <div className="col-4 p-5 text-center">
+          <img src="media/images/pricing0.svg" alt="image" />
+          <h3 >Free equity delivery</h3>
+          <p>All equity delivery investments (NSE, BSE), are absolutely free -₹ 0 brokerage.</p>
+        </div>
+        <div className="col-4 p-5 text-center">
+          <img src="media\images\intradayTrades.svg" alt="image" />
+          <h3>Intraday and F&Q trades</h3>
+          <p>Flat Rs. 20 or 0.035 (whichever is lower) per executed order on intraday trades across equity, currency and commmodity trades.</p>
+        </div>
+        <div className="col-4 p-5 text-center">
+          <img src="media/images/pricingEquity.svg" alt="image" />
+          <h3>Free</h3>
+          <p>All direct mutual fund investments are absolutely free - ₹ 0 commissions & charges.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/landing_page/pricing/PricingPage.jsx
+++ src/landing_page/pricing/PricingPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import Hero from './Hero';
+import OpenAccount from '../OpenAccount';
+import Brokerage from './Brokerage';
 function PricingPage() {
-    return ( <h1>PricingPage</h1> );
+    return ( 
+        <>
+        <Hero />
+        <OpenAccount/>
+        <Brokerage/>
+        </>
+     );
 }
 
 export default PricingPage;
